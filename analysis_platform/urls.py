0355b36ac3ba2cea85acb053994ef98542d1d2a4
Derrick Ampire <ampire90@gmail.com>
2024-08-11 12:27:50 +0300

    fix: removed empty urls for other apps

--- analysis_platform/urls.py
+++ analysis_platform/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('macroeconomiccs.urls')),
-    path('', include('financials.urls')),
 ]
