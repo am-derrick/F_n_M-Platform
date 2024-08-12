7cde500aad556cb3fe3497bf553696f43036905a
Derrick Ampire <ampire90@gmail.com>
2024-08-12 21:38:26 +0300

    fix: added a slash to the url

--- macroeconomics/urls.py
+++ macroeconomics/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/dashboard1', views.dashboard1, name='dashboard1')
+    path('dashboard1/', views.dashboard1, name='dashboard1')
 ]
