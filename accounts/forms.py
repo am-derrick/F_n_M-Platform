8f1a6e24d80acd0685cb03d33f331fe5ee50ecbc
Derrick Ampire <ampire90@gmail.com>
2024-08-14 11:20:42 +0300

    modified: CustomUser model and form added

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import CustomUser
 
 class SignUpForm(UserCreationForm):
     """Form for new user sign up"""
@@ -9,5 +9,5 @@ class SignUpForm(UserCreationForm):
                             widget=forms.EmailInput(attrs={'class': 'form-control'}))
     
     class Meta:
-        model = User
+        model = CustomUser
         fields = ('username', 'email', 'password1', 'password2')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    """user with full memner access"""
+    is_full_member = models.BooleanField(default=False)
