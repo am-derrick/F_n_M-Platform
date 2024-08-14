288bdefdf6228d924e98fa672de2ea513ad5b4eb
Derrick Ampire <ampire90@gmail.com>
2024-08-15 00:01:27 +0300

    modifies: exported app name

--- financials/urls.py
+++ financials/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import financial_analysis_1, financial_analysis_2
 
+app_name = 'financials'
+
 urlpatterns = [
     path('page1/', financial_analysis_1, name='page1'),
     path('page2/', financial_analysis_2, name='page2'),
