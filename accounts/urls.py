4c0aec38d1a74aa5837fa4466bd1d0b1363ba9cf
Derrick Ampire <ampire90@gmail.com>
2024-08-14 11:41:23 +0300

    add: payment logic added and respective urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home, signup, CustomLogoutView, CustomLoginView
+from .views import home, signup, CustomLogoutView, CustomLoginView, create_checkout, payment_cancelled, payment_success
 
 urlpatterns = [
     path('home/', home, name='home'),
     path('signup/', signup, name='signup'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('create_checkout/', create_checkout, name='create_checkout'),
+    path('payment_success/', payment_success, name='payment_success'),
+    path('payment_cancelled/', payment_cancelled, name='payment_cancelled'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
+import stripe
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from .forms import SignUpForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+from django.http import JsonResponse
+from .models import CustomUser
 
 # stripe settings
 stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -32,4 +36,36 @@ def home(request):
     return redirect('macroeconomics:home')
 
 @login_required
-def process_payment(request):
+def create_checkout(request):
+    """creates session on stripe payment checkout"""
+    session = stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': 'Full Membership',
+                },
+                'unit_amount': 1000,
+            },
+            'quantity': 1
+        }],
+        mode='payment',
+        success_url=request.build_absolute_uri('/accounts/payment_success'),
+        cancel_url=request.build_absolute_uri('/accounts/payment_cancelled'),
+    )
+    return JsonResponse({'id': session.id})
+
+@csrf_exempt
+def payment_success(request):
+    """Handles payment success logic"""
+    if request.user.is_authenticated:
+        user = CustomUser.objects.get(username=request.user.username)
+        user.is_full_member = True
+        user.save()
+    return redirect('macroeconomics:home')
+
+@csrf_exempt
+def payment_cancelled(request):
+    """handles payment cancellation"""
+    return redirect('macroeconomics:home')
